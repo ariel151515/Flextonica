29bb5c4b60459fa143f9ef1841db2bed387581d4
ariel151515 <ndcdigital2020@gmail.com>
2024-03-09 16:48:03 -0300

    cree el archivo BoxDiario.js

---
+++ Flextonica-Frontend/components/BoxDiario.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BoxDiario = () => {
+  return (
+    <View>
+      <Text>BoxDiario</Text>
+    </View>
+  )
+}
+
+export default BoxDiario
