09b6207decd4af89e6a62f97c0cf7d6fdb11f63b
ariel151515 <ndcdigital2020@gmail.com>
2024-03-13 13:20:52 -0300

    retoques ultimos

--- Flextonica-Frontend/screens/NutritionTabs/NutrientesScreen.jsx
+++ Flextonica-Frontend/screens/NutritionTabs/NutrientesScreen.jsx
@@ -1,12 +1,142 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+
+
+export const Linea = ({ texto }) => {
+  return (
+    <View>
+      {texto === 'Proteinas' && <View style={styles.lineaP}></View>}
+      {texto === 'Carbohidratos' && <View style={styles.lineaC}></View>}
+      {texto === 'Grasas' && <View style={styles.lineaG}></View>}
+    </View>
+  );
+};
+
+
+export const Item = ({texto,total,objetivo,restan}) => {
+  return (
+      <View style={styles.it}>
+          <View style={styles.item}>
+            <Text style={styles.text}>{texto}</Text>
+            <View style={styles.tres}>
+              <Text style={styles.text}>{total}<Text style={styles.gramos}> g</Text></Text>
+              <Text style={styles.text}>{objetivo}<Text style={styles.gramos}> g</Text></Text>
+              <Text style={styles.text}>{restan}<Text style={styles.gramos}> g</Text></Text>
+            </View>
+          </View>
+
+           <View style={styles.contenedorLinea}>
+              <Linea texto={texto} />
+           </View>
+
+      </View>
+  )
+}
+
 
 const NutrientesScreen = () => {
   return (
-    <View>
-      <Text>NutrientesScreen</Text>
-    </View>
+    <View style={styles.contenedor}>
+      <View style={styles.container}>
+        <View><Text>Total</Text></View>
+        <View><Text>Objetivo</Text></View>
+        <View><Text>Restan</Text></View>
+      </View>
+
+      <Item texto="Proteinas" total="70" objetivo="50" restan="45" />
+      <Item texto="Carbohidratos" total="70" objetivo="50" restan="45" />
+      <Item texto="Grasas" total="70" objetivo="50" restan="45" />
+
+   </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap:20,
+    justifyContent:'flex-end',
+    backgroundColor: '#Fff',
+    paddingRight: 15,
+    paddingLeft: 15,
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+  contenedor:{
+    flex: 1,
+    backgroundColor: '#F5F2F2'
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    gap: 20,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    width: '100%',
+    backgroundColor: '#FFFF',
+    borderTopWidth: 0.5,
+    borderTopColor: '#ccc',
+    marginBottom:0.5
+  },
+  tres: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    gap:38,
+    marginRight:'2%',
+  },
+  gramos: {
+    color: '#ABA8A8',
+  },
+  contenedorGrafico: {
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#FFFF',
+    marginBottom:0.5,
+    marginTop:10
+  },
+  lineaP: {
+    width: '60%',
+    height: 5,
+    backgroundColor: '#54C259',
+    marginBottom:0.5,
+    borderRadius:50
+  },
+  lineaG: {
+    width: '25%',
+    height: 5,
+    backgroundColor: '#EC5F5F',
+    marginBottom:0.5,
+    borderRadius:50
+  },
+  lineaC: {
+    width: '25%',
+    height: 5,
+    backgroundColor: '#0170BF',
+    marginBottom:0.5,
+    borderRadius:50
+  },
+  it:{
+    display: 'flex',
+    flexDirection: 'column',
+    paddingBottom:15,
+    backgroundColor:'#fff',
+  },
+  contenedorLinea: {
+    width: '95%',
+    display: 'flex',
+    marginLeft: 10,
+    backgroundColor:'#E9E5E5',
+    borderRadius:50
+  }
+})
+
 
 export default NutrientesScreen
