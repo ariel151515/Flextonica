31dbe11e363c1981024eb1fd4429d7ce06cd29cf
ariel151515 <ndcdigital2020@gmail.com>
2024-03-13 10:18:37 -0300

    creacion del header nutrition con flecha

--- Flextonica-Frontend/screens/NutricionScreens/NutricionScreens.jsx
+++ Flextonica-Frontend/screens/NutricionScreens/NutricionScreens.jsx
@@ -1,12 +1,45 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 
-const NutricionScreens = () => {
+const Nutrition = () => {
+  const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: '#0B5CFF', // Cambia el color de fondo de la barra superior
+      },
+      headerTintColor: '#fff', // Cambia el color del texto en la barra superior
+      headerLeft: () => (
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <MaterialIcons name="arrow-back" size={24} color="#fff" />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
   return (
-    <View>
-      <Text>NutricionScreens dffd</Text>
+    <View style={styles.container}>
+      <Text>Nutrition</Text>
+      {/* Contenido de la pantalla de nutrición */}
     </View>
-  )
-}
+  );
+};
 
-export default NutricionScreens
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backButton: {
+    marginLeft: 15,
+  },
+});
+
+export default Nutrition;
