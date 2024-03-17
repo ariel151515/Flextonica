6e72fe25fbf8ed0b06a57ed9fdb81a4de6c59b36
ariel151515 <ndcdigital2020@gmail.com>
2024-03-17 16:47:10 -0300

    screen soporte terminada

--- Flextonica-Frontend/screens/SoporteScreen/SoporteScreen.jsx
+++ Flextonica-Frontend/screens/SoporteScreen/SoporteScreen.jsx
@@ -1,50 +1,66 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
 
 const SoporteScreen = () => {
+
+const navigation = useNavigation();
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerStyle: {
+        backgroundColor: '#0B5CFF',
+      },
+      headerTintColor: '#fff',
+      headerLeft: () => (
+        <TouchableOpacity
+          style={styles.backButton}
+          onPress={() => navigation.goBack()}
+        >
+          <MaterialIcons name="arrow-back" size={24} color="#fff" style={{marginLeft:10}} />
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
+
+
   const [asunto, setAsunto] = useState('');
   const [mensaje, setMensaje] = useState('');
-  const [adjunto, setAdjunto] = useState('');
 
   const enviarMensaje = () => {
     // Aquí puedes implementar la lógica para enviar el mensaje, 
     // incluyendo el manejo del archivo adjunto si es necesario
     console.log('Asunto:', asunto);
     console.log('Mensaje:', mensaje);
-    console.log('Archivo adjunto:', adjunto);
   };
+
 
   return (
     <View style={styles.container}>
-      <Text style={styles.label}>Asunto:</Text>
-      <TextInput
-        style={styles.input}
-        value={asunto}
-        onChangeText={setAsunto}
-        placeholder="Ingrese el asunto"
-      />
+        <Text style={styles.label}>Asunto:</Text>
+        <TextInput
+            style={styles.input}
+            value={asunto}
+            onChangeText={setAsunto}
+            placeholder="Ingrese el asunto"
+        />
 
-      <Text style={styles.label}>Mensaje:</Text>
-      <TextInput
-        style={[styles.input, styles.multilineInput]}
-        value={mensaje}
-        onChangeText={setMensaje}
-        placeholder="Ingrese el mensaje"
-        multiline
-        numberOfLines={4}
-      />
+        <Text style={styles.label}>Mensaje:</Text>
+        <TextInput
+            style={[styles.input, styles.multilineInput]}
+            value={mensaje}
+            onChangeText={setMensaje}
+            placeholder="Ingrese el mensaje"
+            multiline
+            numberOfLines={4}
+        />
 
-      <Text style={styles.label}>Adjuntar archivo:</Text>
-      <TextInput
-        style={styles.input}
-        value={adjunto}
-        onChangeText={setAdjunto}
-        placeholder="Seleccione el archivo adjunto"
-      />
 
-      <TouchableOpacity style={styles.button} onPress={enviarMensaje}>
-        <Text style={styles.buttonText}>Enviar</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={enviarMensaje}>
+            <Text style={styles.buttonText}>Enviar</Text>
+        </TouchableOpacity>
       
     </View>
   );
