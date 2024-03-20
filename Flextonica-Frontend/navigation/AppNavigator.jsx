bce9f13b7e2264741e737fba3fa86fa09bf63634
ariel151515 <ndcdigital2020@gmail.com>
2024-03-20 11:51:51 -0300

    tab y stack en archivos separados, funciona perfecto

--- Flextonica-Frontend/navigation/AppNavigator.jsx
+++ Flextonica-Frontend/navigation/AppNavigator.jsx
@@ -1,11 +1,8 @@
 // AppNavigator.js
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Feather, Ionicons, MaterialCommunityIcons, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
 
 // Screen
 import DayScreen from './../screens/DayScreen/DayScreen';
@@ -24,106 +21,9 @@ import EvolutionScreen from '../screens/EvolutionScreen/EvolutionScreen';
 import AddSeguimiento from '../screens/EvolutionScreen/AddSeguimiento';
 import ObjetivosScreen from '../screens/ObjetivosScreen/ObjetivosScreen';
 
+import TabNavigator from './TabNavigator';
 
-const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
-
-const TabNavigator = () => {
-  return (
-    <Tab.Navigator 
-          screenOptions={{
-            tabBarActiveTintColor: '#6200EE',
-            tabBarInactiveTintColor: '#A4A0A0',
-            tabBarStyle: [{ 
-                    display: 'flex', 
-                    backgroundColor:'#fff',
-                    height:'7%'
-                },
-                null
-            ]
-        }}
-    >
-      <Tab.Screen 
-           name="Day" 
-           component={DayScreen} 
-           options={{
-            tabBarIcon: ({ focused, color, size }) => (
-                <Ionicons 
-                     name="today-outline" 
-                     size={size} 
-                     color={color}
-                    />
-            ),
-            tabBarLabel: ({ focused, color }) => (
-                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
-                    Day
-                </Text>
-            ),
-            headerShown: false,
-            headerTitle: false
-        }}
-           />
-
-      <Tab.Screen 
-           name="Week" 
-           component={WeekScreen} 
-           options={{
-            tabBarIcon: ({ focused, color, size }) => (
-                <MaterialCommunityIcons 
-                          name="calendar-week-begin" 
-                          size={size} 
-                          color={color} />
-            ),
-            tabBarLabel: ({ focused, color }) => (
-                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
-                    Week
-                </Text>
-            )
-
-        }}
-           />
-
-      <Tab.Screen 
-           name="Nutrition" 
-           component={NutritionTabs} 
-           options={{
-            tabBarIcon: ({ focused, color, size }) => (
-                <FontAwesome5 
-                     name="nutritionix" 
-                     size={size} 
-                     color={color}
-                      />
-            ),
-            tabBarLabel: ({ focused, color }) => (
-                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
-                    Nutrition
-                </Text>
-            )
-        }}
-           />
-
-
-      <Tab.Screen 
-           name="Menu" 
-           component={MenuScreen} 
-           options={{
-            tabBarIcon: ({ focused, color, size }) => (
-                <Feather 
-                    name="menu" 
-                    size={size} 
-                    color={color}/>
-            ),
-            tabBarLabel: ({ focused, color }) => (
-                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
-                    Menu
-                </Text>
-            ),
-        }}
-           />
-
-    </Tab.Navigator>
-  );
-}
 
 const AppNavigator = () => {
   return (
---
+++ Flextonica-Frontend/navigation/TabNavigator.jsx
@@ -0,0 +1,114 @@
+// AppNavigator.js
+import React from 'react';
+import { Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather, Ionicons, MaterialCommunityIcons, FontAwesome5, MaterialIcons } from '@expo/vector-icons';
+
+// Screen
+import DayScreen from '../screens/DayScreen/DayScreen';
+import WeekScreen from '../screens/WeekScreen/WeekScreen';
+import MenuScreen from '../screens/MenuScreen/MenuScreen';
+import NutritionTabs from '../screens/NutritionTabs/NutritionTabs';
+
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator 
+          screenOptions={{
+            tabBarActiveTintColor: '#6200EE',
+            tabBarInactiveTintColor: '#A4A0A0',
+            tabBarStyle: [{ 
+                    display: 'flex', 
+                    backgroundColor:'#fff',
+                    height:'7%'
+                },
+                null
+            ]
+        }}
+    >
+      <Tab.Screen 
+           name="Day" 
+           component={DayScreen} 
+           options={{
+            tabBarIcon: ({ focused, color, size }) => (
+                <Ionicons 
+                     name="today-outline" 
+                     size={size} 
+                     color={color}
+                    />
+            ),
+            tabBarLabel: ({ focused, color }) => (
+                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
+                    Day
+                </Text>
+            ),
+            headerShown: false,
+            headerTitle: false
+        }}
+           />
+
+      <Tab.Screen 
+           name="Week" 
+           component={WeekScreen} 
+           options={{
+            tabBarIcon: ({ focused, color, size }) => (
+                <MaterialCommunityIcons 
+                          name="calendar-week-begin" 
+                          size={size} 
+                          color={color} />
+            ),
+            tabBarLabel: ({ focused, color }) => (
+                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
+                    Week
+                </Text>
+            )
+
+        }}
+           />
+
+      <Tab.Screen 
+           name="Nutrition" 
+           component={NutritionTabs} 
+           options={{
+            tabBarIcon: ({ focused, color, size }) => (
+                <FontAwesome5 
+                     name="nutritionix" 
+                     size={size} 
+                     color={color}
+                      />
+            ),
+            tabBarLabel: ({ focused, color }) => (
+                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
+                    Nutrition
+                </Text>
+            )
+        }}
+           />
+
+
+      <Tab.Screen 
+           name="Menu" 
+           component={MenuScreen} 
+           options={{
+            tabBarIcon: ({ focused, color, size }) => (
+                <Feather 
+                    name="menu" 
+                    size={size} 
+                    color={color}/>
+            ),
+            tabBarLabel: ({ focused, color }) => (
+                <Text style={{ fontSize: 12, marginBottom:0, color: color, marginTop:0}}>
+                    Menu
+                </Text>
+            ),
+        }}
+           />
+
+    </Tab.Navigator>
+  );
+}
+
+
+export default TabNavigator;
