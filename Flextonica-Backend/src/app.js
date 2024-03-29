daac8c458b96b30a6ba8e0e5b62a23724e1098ff
ariel151515 <ndcdigital2020@gmail.com>
2024-03-29 18:57:39 -0300

    retoques

--- Flextonica-Backend/src/app.js
+++ Flextonica-Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-
+// app.use(express.json());
 
 // Definir una ruta GET para la raíz "/"
 app.get('/', (req, res) => {
